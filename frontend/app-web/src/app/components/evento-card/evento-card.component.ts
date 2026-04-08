2bd3f55f83a3d585049989d5f8a3300474785f5f
Francisco Jose Soria <franciscosorianavarrete@gmail.com>
2026-04-08 18:47:05 +0200

    Frontend inicial

---
+++ frontend/app-web/src/app/components/evento-card/evento-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evento-card',
+  imports: [],
+  templateUrl: './evento-card.component.html',
+  styleUrl: './evento-card.component.css'
+})
+export class EventoCardComponent {
+
+}
